90bf5417b7b4bfae587abe5ddf39a0fb8df0dfd6
Yaroslav Yakymiv <yakymiv.0706@gmail.com>
2021-06-27 18:47:00 +0300

    update

---
+++ src/config/firebase-conf.js
@@ -0,0 +1,10 @@
+const firebaseObject = {
+  apiKey: "AIzaSyDQW5jhcT06sBwihy4wA2LKqipwO11o99o",
+  authDomain: "yakymiv-product-list.firebaseapp.com",
+  projectId: "yakymiv-product-list",
+  storageBucket: "yakymiv-product-list.appspot.com",
+  messagingSenderId: "631754169498",
+  appId: "1:631754169498:web:76f0d58bd96bb02a2d5323"
+}
+
+export default firebaseObject;
